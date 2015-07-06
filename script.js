c03117395261ecd65b4dbaa03423a5b0701880a4
Austin Adams <austin@farmingtonwebdesign.net>
2015-07-06 14:27:11 -0700

    Added a controller

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 // Code goes here
-
-angular.module('myApp', []);
+angular.module('myApp', [])
+  .controller("storyController", function ($scope) {
+    $scope.name,
+      $scope.job_title,
+      $scope.tedious_task,
+      $scope.celebrity,
+      $scope.dirty_task,
+      $scope.useless_skill,
+      $scope.adjective,
+      $scope.obnoxiuous_celbertity,
+      $scope.huge_number = "";
+    $scope.gender = 'F';
+    $scope.genderStrFn = function(){
+      $scope.genderStr1 = $scope.gender === 'F' ? "her":"his";
+      $scope.genderStr2 = $scope.gender === 'F' ? "she":"he";
+      $scope.genderStr3 = $scope.gender === 'F' ? "her":"him";
+    };
+    $scope.genderStrFn();
+  });
