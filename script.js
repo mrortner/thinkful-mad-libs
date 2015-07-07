2471b19d3e73ea4fd01b0e7b04bab99a77f11450
mrortner <mrortner@gmail.com>
2015-07-07 09:18:07 -0700

    Controller Modify

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
 // Code goes here
 angular.module('myApp', [])
-  .controller("storyController", function ($scope) {
-    $scope.name,
-      $scope.job_title,
-      $scope.tedious_task,
-      $scope.celebrity,
-      $scope.dirty_task,
-      $scope.useless_skill,
-      $scope.adjective,
-      $scope.obnoxiuous_celbertity,
-      $scope.huge_number = "";
-    $scope.gender = 'F';
-    $scope.genderStrFn = function(){
-      $scope.genderStr1 = $scope.gender === 'F' ? "her":"his";
-      $scope.genderStr2 = $scope.gender === 'F' ? "she":"he";
-      $scope.genderStr3 = $scope.gender === 'F' ? "her":"him";
-    };
-    $scope.genderStrFn();
-  });
+    .controller("storyController", function ($scope) {
+        $scope.answers = {
+            "name": "",
+            "job_title": "",
+            "tedious_task": "",
+            "celebrity":"",
+            "dirty_task":"",
+            "useless_skill":"",
+            "adjective":"",
+            "obnoxious_celebrity":"",
+            "huge_number":"",
+            "gender":""
+        };
+        $scope.genderSelection = function(){
+            $scope.gender1 = $scope.gender === "F" ? "her" : "his";
+            $scope.gender2 = $scope.gender === 'F' ? "she" : "he";
+            $scope.gender3 = $scope.gender === 'F' ? "her" : "him";
+        }
+        $scope.genderSelection();
+
+    });
